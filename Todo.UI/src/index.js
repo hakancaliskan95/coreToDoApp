25b74c18c5a4e3598c7f835c2a197ed4b0cc070e
Hakan Çalışkan <hakancaliskan95@gmail.com>
2018-07-08 13:44:43 +0300

    Revert "Develop"

--- Todo.UI/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import "./css/index.css";
-
-const container = document.getElementById('app');
-container && ReactDOM.render(<App />, container);
